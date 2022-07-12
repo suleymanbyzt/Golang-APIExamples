409c286bf84124b27cdd2694d987da338b97268a
Süleyman Bayazit <suleymanbayazitt@gmail.com>
2022-07-12 17:18:04 +0300

    golang examples

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"io/ioutil"
+	"net/http"
+)
+
+type Order struct {
+	PairSymbol string  "json:'pairSymbol'"
+	OrderType  string  "json:'orderType'"
+	Quantity   float64 "json:'pairSymbol'"
+	Price      float64 "json:'price'"
+}
+
+func getOrder(c echo.Context) error {
+	return c.String(http.StatusOK, "GET request")
+}
+
+func createOrder(c echo.Context) error {
+	order := Order{}
+
+	body, err := ioutil.ReadAll(c.Request().Body)
+
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(body, &order)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(order)
+	return c.String(http.StatusCreated, "Order Created!")
+}
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+
+	e.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+		if username == "guest" && password == "admin" {
+			return true, nil
+		}
+
+		return false, nil
+	}))
+
+	orderGroup := e.Group("/v1/order")
+	orderGroup.GET("/", getOrder)
+	orderGroup.POST("/", createOrder)
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
